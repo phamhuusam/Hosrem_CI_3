e2c97bd08d1517878a1bbb2f58109dfedfa9b402
tranbinhtam <binhtam@ueh.edu.vn>
2017-03-23 22:18:39 +0700

    dfdf

---
+++ application/views/front_end/danhba.php
@@ -0,0 +1,157 @@
+<div id="danhbawrapper">
+	<div class="db-header">
+		<div class="search-box">
+			<form action="" method="post">
+				<input class="keys" type="text" name="keys" placeholder="Tìm kiếm bác sĩ"><br />
+				<input class="button" type="submit" value="Đăng ký danh bạ bác sĩ" name="search">
+			</form>
+		</div>
+	</div>
+	<div class="clear"></div>
+	<div id="ca-container" class="ca-container">
+		<div class="ca-wrapper carousel">		
+				<div class="bacsi-box ca-item ca-item-1">
+					<div class="ca-item-main">
+						<img src="<?php echo base_url() ?>public/images/bacsi.jpg" />
+						<span class="title">
+							<h2 class="tenbs">GS. TS <strong>NGUYỄN TRẦN LÂM 1</strong></h2>
+							<h3 class="noicongtac">Bệnh viện CHỢ RẪY</h3>
+							<span class="muiten"><a href="#"><img src="<?php echo base_url() ?>public/images/arrow.png"></a></span>
+						</span>
+					</div>
+				</div>
+				
+				<div class="bacsi-box ca-item ca-item-2">
+					<div class="ca-item-main">
+						<img src="<?php echo base_url() ?>public/images/bacsi.jpg" />
+						<span class="title">
+							<h2 class="tenbs">GS. TS <strong>NGUYỄN TRẦN LÂM 2</strong></h2>
+							<h3 class="noicongtac">Bệnh viện CHỢ RẪY</h3>
+							<span class="muiten"><a href="#"><img src="<?php echo base_url() ?>public/images/arrow.png"></a></span>
+						</span>
+					</div>
+				</div>
+				<div class="bacsi-box ca-item ca-item-3">
+					<div class="ca-item-main">
+						<img src="<?php echo base_url() ?>public/images/bacsi.jpg" />
+						<span class="title">
+							<h2 class="tenbs">GS. TS <strong>NGUYỄN TRẦN LÂM 3</strong></h2>
+							<h3 class="noicongtac">Bệnh viện CHỢ RẪY</h3>
+							<span class="muiten"><a href="#"><img src="<?php echo base_url() ?>public/images/arrow.png"></a></span>
+						</span>
+					</div>
+				</div>
+				<div class="bacsi-box ca-item ca-item-4">
+					<div class="ca-item-main">
+						<img src="<?php echo base_url() ?>public/images/bacsi.jpg" />
+						<span class="title">
+							<h2 class="tenbs">GS. TS <strong>NGUYỄN TRẦN LÂM 4 </strong></h2>
+							<h3 class="noicongtac">Bệnh viện CHỢ RẪY</h3>
+							<span class="muiten"><a href="#"><img src="<?php echo base_url() ?>public/images/arrow.png"></a></span>
+						</span>
+					</div>
+				</div>
+				<div class="bacsi-box ca-item ca-item-5">
+					<div class="ca-item-main">
+						<img src="<?php echo base_url() ?>public/images/bacsi.jpg" />
+						<span class="title">
+							<h2 class="tenbs">GS. TS <strong>NGUYỄN TRẦN LÂM 5</strong></h2>
+							<h3 class="noicongtac">Bệnh viện CHỢ RẪY</h3>
+							<span class="muiten"><a href="#"><img src="<?php echo base_url() ?>public/images/arrow.png"></a></span>
+						</span>
+					</div>
+				</div>				
+				<div class="bacsi-box ca-item ca-item-6">
+					<div class="ca-item-main">
+						<img src="<?php echo base_url() ?>public/images/bacsi.jpg" />
+						<span class="title">
+							<h2 class="tenbs">GS. TS <strong>NGUYỄN TRẦN LÂM 6</strong></h2>
+							<h3 class="noicongtac">Bệnh viện CHỢ RẪY</h3>
+							<span class="muiten"><a href="#"><img src="<?php echo base_url() ?>public/images/arrow.png"></a></span>
+						</span>
+					</div>
+				</div>
+				<div class="bacsi-box ca-item ca-item-7">
+					<div class="ca-item-main">
+						<img src="<?php echo base_url() ?>public/images/bacsi.jpg" />
+						<span class="title">
+							<h2 class="tenbs">GS. TS <strong>NGUYỄN TRẦN LÂM 7</strong></h2>
+							<h3 class="noicongtac">Bệnh viện CHỢ RẪY</h3>
+							<span class="muiten"><a href="#"><img src="<?php echo base_url() ?>public/images/arrow.png"></a></span>
+						</span>
+					</div>
+				</div>
+				
+				<div class="bacsi-box ca-item ca-item-8">
+					<div class="ca-item-main">
+						<img src="<?php echo base_url() ?>public/images/bacsi.jpg" />
+						<span class="title">
+							<h2 class="tenbs">GS. TS <strong>NGUYỄN TRẦN LÂM 8</strong></h2>
+							<h3 class="noicongtac">Bệnh viện CHỢ RẪY</h3>
+							<span class="muiten"><a href="#"><img src="<?php echo base_url() ?>public/images/arrow.png"></a></span>
+						</span>
+					</div>
+				</div>
+				<div class="bacsi-box ca-item ca-item-9">
+					<div class="ca-item-main">
+						<img src="<?php echo base_url() ?>public/images/bacsi.jpg" />
+						<span class="title">
+							<h2 class="tenbs">GS. TS <strong>NGUYỄN TRẦN LÂM 9</strong></h2>
+							<h3 class="noicongtac">Bệnh viện CHỢ RẪY</h3>
+							<span class="muiten"><a href="#"><img src="<?php echo base_url() ?>public/images/arrow.png"></a></span>
+						</span>
+					</div>
+				</div>
+				<div class="bacsi-box ca-item ca-item-10">
+					<div class="ca-item-main">
+						<img src="<?php echo base_url() ?>public/images/bacsi.jpg" />
+						<span class="title">
+							<h2 class="tenbs">GS. TS <strong>NGUYỄN TRẦN LÂM 10</strong></h2>
+							<h3 class="noicongtac">Bệnh viện CHỢ RẪY</h3>
+							<span class="muiten"><a href="#"><img src="<?php echo base_url() ?>public/images/arrow.png"></a></span>
+						</span>
+					</div>
+				</div>
+				<div class="bacsi-box ca-item ca-item-11">
+					<div class="ca-item-main">
+						<img src="<?php echo base_url() ?>public/images/bacsi.jpg" />
+						<span class="title">
+							<h2 class="tenbs">GS. TS <strong>NGUYỄN TRẦN LÂM 11</strong></h2>
+							<h3 class="noicongtac">Bệnh viện CHỢ RẪY</h3>
+							<span class="muiten"><a href="#"><img src="<?php echo base_url() ?>public/images/arrow.png"></a></span>
+						</span>
+					</div>
+				</div>				
+				<div class="bacsi-box ca-item ca-item-12">
+					<div class="ca-item-main">
+						<img src="<?php echo base_url() ?>public/images/bacsi.jpg" />
+						<span class="title">
+							<h2 class="tenbs">GS. TS <strong>NGUYỄN TRẦN LÂM 12</strong></h2>
+							<h3 class="noicongtac">Bệnh viện CHỢ RẪY</h3>
+							<span class="muiten"><a href="#"><img src="<?php echo base_url() ?>public/images/arrow.png"></a></span>
+						</span>
+					</div>
+				</div>				
+		</div>
+	</div>
+</div>
+  <script src="https://code.jquery.com/jquery-2.2.0.min.js" type="text/javascript"></script>
+  <script src="<?php echo base_url() ?>public/slick/slick.js" type="text/javascript" charset="utf-8"></script>
+    <script type="text/javascript">
+    $(document).on('ready', function() {
+     $('.carousel').slick({
+		dots: true,
+		slidesPerRow: 3,
+		rows: 2,
+		responsive: [
+			{
+			  breakpoint: 478,
+			  settings: {
+				slidesPerRow: 1,
+				rows: 1,
+			  }
+			}
+		]
+		});
+      });
+	  </script>
