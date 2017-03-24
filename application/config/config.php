bf717f401294170d8dd0cef509335ef1ccef75db
phamhuusam <phamhuusam@gmail.com>
2017-03-24 10:18:22 +0700

    fix index.

--- application/config/config.php
+++ application/config/config.php
@@ -35,7 +35,7 @@ $config['base_url'] = 'http://localhost:8080/hosrem_new/trunk';
 | variable so that it is blank.
 |
  */
-$config['index_page'] = 'index.php';
+$config['index_page'] = '';
 
 /*
 |--------------------------------------------------------------------------
